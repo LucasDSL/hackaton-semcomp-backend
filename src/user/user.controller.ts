ba8f5abe57a689504613c9bb35491016c797eb98
LucasDSL <lucasdslima2002@gmail.com>
2022-09-01 01:51:29 -0300

    feat: user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,35 +8,37 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Prisma } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
+    return this.userService.createUser(createUserDto);
   }
 
   @Get()
   findAll() {
-    return this.userService.findAll();
+    return this.userService.users({
+      /* PUT BODY FROM userService.users HERE */
+    });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.user({ id: Number(id) });
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.updateUser({ where: { id: Number(id) }, data: {} });
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
